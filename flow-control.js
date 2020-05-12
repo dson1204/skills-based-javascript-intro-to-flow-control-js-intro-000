57a1aeb16957608a86053fd9c169c6f0ab128ae3
Daniel Son <dson1204@gmail.com>
2020-05-12 02:00:12 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -11,9 +11,7 @@ else {return `You are a teenager!`}
 }
 
 function ageChecker(age) {
-if (age <13) {return `You are a kid`}
-else if (age > 19) {return `You are a grownup`}
-else (age >= 13 || age <= 19) {return `You are a teenager!`}
+
 }
 
 function ternaryTeenager(age) {
